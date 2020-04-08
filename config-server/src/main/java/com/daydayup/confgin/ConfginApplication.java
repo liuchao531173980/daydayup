c44c35e8907d87f86cebd841f4ac3cda9f200115
86183 <531173980@qq.com>
2020-04-08 21:28:22 +0800

    sdsdss

---
+++ config-server/src/main/java/com/daydayup/confgin/ConfginApplication.java
@@ -0,0 +1,13 @@
+package com.daydayup.confgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfginApplication.class, args);
+	}
+
+}
