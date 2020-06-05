08a4c53f1bdaa734fb6bde8a7c9b78e8d70381f3
liuchao <liuchao2@9191offer.com>
2020-06-05 16:44:26 +0800

    .

--- daybegin/src/main/java/com/daydayup/daybegin/DaybeginApplication.java
+++ daybegin/src/main/java/com/daydayup/daybegin/DaybeginApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ConfginApplication {
+public class DaybeginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfginApplication.class, args);
+		SpringApplication.run(DaybeginApplication.class, args);
 	}
 
 }
